0c1e2a31f222b4914150354c10a70bada378934b
smithua <smithuaa@gmail.com>
2014-07-29 12:24:31 +0300

    fix content look

--- wp-content/themes/ooo-pulse/sidebar.php
+++ wp-content/themes/ooo-pulse/sidebar.php
@@ -5,11 +5,11 @@
  * @package ooo-pulse
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
+//if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+//	return;
+//}
 ?>
 
-<div id="secondary" class="widget-area" role="complementary">
+<!--<div id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+</div>--><!-- #secondary -->
